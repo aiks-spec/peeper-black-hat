fdc8262c537bc252967b5345dcb015aa8a062053
Aiks <commondrive2024@gmail.com>
2025-09-02 00:04:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Simple entrypoint - no tput or colors.sh handling needed
+# Prevent sourcing of shell configuration files
+
+# Disable shell configuration loading
+set +o allexport
+unset BASH_ENV
+unset ENV
 
 # Start the application
 exec "$@"
